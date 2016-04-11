99ca6dad9c121840a75c8a19ff4496c571cd298b
thoum <indiofish@gmail.com>
2016-04-11 22:52:12 +0900

    fix setup script

--- nvim/setup.sh
+++ nvim/setup.sh
@@ -1,14 +1,14 @@
 #/bin/bash
 
 echo "This script replaces the current vim settings!"
-NVIM_PATH=~/.config/nvim
+NVIM_PATH="$HOME/.config/nvim"
 
-rm -rf NVIM_PATH
-mkdir NVIM_PATH
-cp init.vim NVIM_PATH/init.vim
-cp -r syntax NVIM_PATH/syntax
+rm -rf "$NVIM_PATH"
+mkdir "$NVIM_PATH"
+cp init.vim "$NVIM_PATH"/init.vim
+cp -r syntax "$NVIM_PATH"/syntax
 
-curl -fLo NVIM_PATH/autoload/plug.vim --create-dirs \
+curl -fLo "$NVIM_PATH"/autoload/plug.vim --create-dirs \
       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 nvim ./README.md +PlugInstall +qall
